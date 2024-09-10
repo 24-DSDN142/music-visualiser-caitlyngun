07abc228bed6680a10db9ab99a5c97fded6c5081
caitlyngun <gundercait@myvuw.ac.nz>
2024-09-10 19:02:46 +1200

    Changed size of everything, created maps

--- music_starter.js
+++ music_starter.js
@@ -1,60 +1,99 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
-// function setup () {
-//    createcanvas (500,500);
-// }
-// function draw() {
-// background (0)
 
-// draw_one_frame()
-// }
+function setup() {
+  createCanvas (400,400) ; 
+  background(170);
+  strokeWeight(4);
+}
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-background(9, 12, 46) //darkblue
+background(0,0,0) //black
 //textFont('verdana')
-// rectMode(center)
+
 textSize(24)
 
 
 
-// let y = 100; 
-
-
-//  for (let i = 1; i <= 6; i ++)
-//   {
-//  fill (0)
-//   let loopyY = y  * i;
-//   fill(255)
-//   ellipse (400, loopyY, 100) 
-//   }
-
-
+//part at bottom of rect
 stroke(255, 23, 151) //pink
-strokeWeight(8)
-line(100,448,150,400)
-line (450,448,400,400)
-line(150,400,400,400)
+strokeWeight(3)
+line(500,600,600,550)
+line (950,600,850,550)
+line(600,550,850,550)
 
-//radio outline
-strokeWeight(4)
+//radio outline - darkest colours to brightest
+strokeWeight(15)
+stroke(36, 6, 23)
+rect(425,300,600,300)
+
+strokeWeight(12)
+stroke(61, 11, 39)
+rect(425,300,600,300)
+
+strokeWeight(12)
+stroke(112, 22, 72)
+rect(425,300,600,300)
+
+strokeWeight(9)
+stroke(150, 30, 96)
+rect (425,300,600,300)
+
+strokeWeight(6)
+stroke(179, 36, 114)
+rect (425,300,600,300)
+
+//brightpink top radio outline
+strokeWeight(3)
 stroke(255, 23, 151) //bright pink
 noFill()
-rect (75,250,400,200)
+rect (425,300,600,300)
+
+
 
 //speakers 
-fill(64, 104, 247)
-stroke(64, 104, 247) //blue
-strokeWeight(7)
-circle(180,350,140) //left
-circle (370,350,75) //right
+fill(255,255,255)
+noStroke()
 
-//inside speaker
+//left speaker
+let leftCircleSize = map(drum,0,100, 50,200)
+circle(655,400,leftCircleSize)
+
+//right speaker
+let rightCircleSize = map(bass,0,100,10,90)
+circle (845,400,rightCircleSize) 
+
+
+//inside speakers
+
+//left inside
+let insideLeftCirc = map (bass,0,100,5,80)
 noFill()
-stroke(200, 89, 255) //purple
-strokeWeight(4)
-circle(180,350,100) //left
-circle(370,350,45)
+stroke(255, 23, 151)
+strokeWeight(3)
+circle (655,400, insideLeftCirc) 
 
+//right inside
+let insideRightCirc = map (drum,0,100,5,60)
+noFill()
+stroke(255, 23, 151)
+strokeWeight(3)
+circle(845,400,insideRightCirc) 
+
+
+
+//inside rect
+noFill()
+stroke(255, 23, 151) //pink
+strokeWeight(5)
+rect(550,350,350,100) 
+
+//little corner circles
+strokeWeight(2)
+circle(440,315,10) //left top
+circle(440,585,10) //left bottom
+circle(1010,315,10) //right top
+circle(1010,585,10) //right bottom
 
 
 }
